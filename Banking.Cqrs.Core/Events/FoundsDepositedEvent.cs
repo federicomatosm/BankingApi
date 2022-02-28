aceba5ccbb12a9498631fa872e75c3d97678a3ea
Federico Matos <federicomatos@icloud.com>
2022-02-28 19:58:27 -0400

    feature: create events and event sourcing

---
+++ Banking.Cqrs.Core/Events/FoundsDepositedEvent.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Banking.Cqrs.Core.Events
+{
+    public class FoundsDepositedEvent : BaseEvent
+    {
+        public double Amount { get; set; }
+
+        public FoundsDepositedEvent(string id) : base(id)
+        {
+        }
+    }
+}
+
---
+++ Banking.Cqrs.Core/Events/FoundsWithdrawnEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Banking.Cqrs.Core.Events
+{
+	public class FoundsWithdrawnEvent : BaseEvent
+	{
+        public double Amount { get; set; }
+
+        public FoundsWithdrawnEvent(string id) : base(id)
+        {
+        }
+    }
+}
+
