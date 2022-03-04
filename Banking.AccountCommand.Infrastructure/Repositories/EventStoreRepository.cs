8b28cd93b91d43da14f9576e3d320165ff322499
Federico Matos <federicomatos@icloud.com>
2022-03-03 23:02:51 -0400

    feature: Microservice command
    Added infrastructure project, EventStore repository and MongoRepository

---
+++ Banking.AccountCommand.Infrastructure/Repositories/EventStoreRepository.cs
@@ -0,0 +1,25 @@
+﻿
+using Banking.Account.Command.Domain;
+using Banking.Account.Command.Application.Contracts.Persistence;
+using Microsoft.Extensions.Options;
+using Banking.Account.Command.Application.Models;
+using MongoDB.Driver;
+
+namespace Banking.AccountCommand.Infrastructure.Repositories
+{
+	public class EventStoreRepository : MongoRepository<EventModel>, IEventStoreRepository
+	{
+
+        public EventStoreRepository(IOptions<MongoSettings> options) : base(options)
+        {
+        }
+
+        public async Task<IEnumerable<EventModel>> FindByAggregateIdentifier(string aggregateIdentifier)
+        {
+            FilterDefinition<EventModel>? filter = Builders<EventModel>.Filter.Eq(doc => doc.AggregateIdentifier, aggregateIdentifier);
+
+            return await _collection.Find(filter).ToListAsync();
+        }
+    }
+}
+
