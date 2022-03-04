e1b339fb79f9455e9604d663418b4fef85cc5ef3
Federico Matos <federicomatos@icloud.com>
2022-03-03 22:39:00 -0400

    feature: Microservice Command
    Added Application project, interfaces, kafka and mongodb settings,

---
+++ Banking.Account.Command.Application/Contracts/Persistence/IEventStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Banking.Account.Command.Domain;
+
+namespace Banking.Account.Command.Application.Contracts.Persistence
+{
+	public interface IEventStoreRepository : IMongoRepository<EventModel>
+	{
+
+		Task<IEnumerable<EventModel>> FindByAggregateIdentifier(string aggregateIdentifier);
+	}
+}
+
