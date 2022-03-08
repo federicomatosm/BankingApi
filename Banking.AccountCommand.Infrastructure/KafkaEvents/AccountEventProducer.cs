379232d63f7b1e1485918f4a12db57b1893ef21a
Federico Matos <federicomatos@icloud.com>
2022-03-08 06:26:07 -0400

    feature: Microservice command
    Added AccountEventProducer

--- Banking.AccountCommand.Infrastructure/KafkaEvents/AccountEventProducer.cs
+++ Banking.AccountCommand.Infrastructure/KafkaEvents/AccountEventProducer.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Banking.Account.Command.Application.Models;
+using Banking.Cqrs.Core.Events;
+using Banking.Cqrs.Core.Producers;
+using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
 namespace Banking.AccountCommand.Infrastructure.KafkaEvents
 {
-	public class AccountEventProducer
+	public class AccountEventProducer : IEventProducer
 	{
-		public AccountEventProducer()
-		{
-		}
-	}
+        public KafkaSettings _kafkaSettings { get; set; }
+
+        public AccountEventProducer(IOptions<KafkaSettings> kafkaSettings)
+        {
+            _kafkaSettings = kafkaSettings.Value;
+        }
+
+        public void Produce(string topic, BaseEvent @event)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = $"{_kafkaSettings.Hostname}:{_kafkaSettings.Port}"
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            var classEvent = @event.GetType();
+            string value = JsonConvert.SerializeObject(@event);
+
+            var message = new Message<Null, string> { Value = value };
+
+            producer.ProduceAsync(topic, message).GetAwaiter().GetResult();
+        }
+    }
 }
 
