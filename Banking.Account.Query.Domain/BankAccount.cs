43d57f6721e68daac72058d8e33417495ca29414
Federico Matos <federicomatos@icloud.com>
2022-03-23 22:42:18 -0400

    feature: microservice query
    Added API query  and clean code architecture

---
+++ Banking.Account.Query.Domain/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Banking.Account.Query.Domain.Common;
+
+namespace Banking.Account.Query.Domain
+{
+	public class BankAccount : BaseDomainModel
+	{
+        public string Identifier { get; set; } = string.Empty;
+        public string AccountHolder { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+        public string AccountType { get; set; } = string.Empty;
+        public double Balance { get; set; }
+    }
+}
+
